f4dcc86fd304930245961cf695885471731b14bc
Ruslan <ruslan777mirov@gmail.com>
2019-08-20 11:35:00 +0300

    Updated android link

--- src/components/static/Web3Info.js
+++ src/components/static/Web3Info.js
@@ -9,7 +9,7 @@ function Web3Info(props) {
      props.isDataLoad && !props.web3
      ?
      (
-      <Alert variant="warning">To actually make trades, you'll need to use <strong>Metamask</strong> for <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">web</a> or <a href="https://addons.mozilla.org/ru/android/addon/ether-metamask/" target="_blank" rel="noopener noreferrer">Android</a>  or <strong>TrustWallet</strong> for <a href="https://apps.apple.com/us/app/trust-ethereum-wallet/id1288339409" target="_blank" rel="noopener noreferrer">IPhone</a> or <a href="https://play.google.com/store/apps/details?id=com.wallet.crypto.trustapp" target="_blank" rel="noopener noreferrer">Android</a> </Alert>
+      <Alert variant="warning">To actually make trades, you'll need to use <strong>Metamask</strong> for <a href="https://metamask.io/" target="_blank" rel="noopener noreferrer">web</a> or <a href="https://play.google.com/store/apps/details?id=io.metamask" target="_blank" rel="noopener noreferrer">Android</a>  or <strong>TrustWallet</strong> for <a href="https://apps.apple.com/us/app/trust-ethereum-wallet/id1288339409" target="_blank" rel="noopener noreferrer">IPhone</a> or <a href="https://play.google.com/store/apps/details?id=com.wallet.crypto.trustapp" target="_blank" rel="noopener noreferrer">Android</a> </Alert>
      )
      :
      (null)
