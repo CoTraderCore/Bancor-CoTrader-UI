2960368b10a75bbaedfefb1ac2d9b85b92c93c03
Ruslan <ruslan777mirov@gmail.com>
2019-08-20 14:07:00 +0300

    Make Deriction info view without web3 + refactoring

--- src/components/converter/modals/modules/DirectionInfo.js
+++ src/components/converter/modals/modules/DirectionInfo.js
@@ -9,6 +9,7 @@ import {
 } from '../../../../config'
 import getDirectionData from '../../../../service/getDirectionData'
 import getPath from '../../../../service/getPath'
+import getWeb3ForRead from '../../../../service/getWeb3ForRead'
 
 
 class DirectionInfo extends Component {
@@ -33,6 +34,7 @@ class DirectionInfo extends Component {
     }
   }
 
+  // get user balance
   getTokensBalance = async (sendFrom, sendTo, web3) => {
     let userBalanceFrom
     let token
@@ -53,69 +55,47 @@ class DirectionInfo extends Component {
     return { userBalanceFrom, balanceOfTo }
   }
 
-  getRateInfo = async (objPropsFrom, objPropsTo, web3) => {
+  // return rate from Bancor network
+  getReturnByPath = async (path, amount, web3) => {
     const bancorNetwork = web3.eth.Contract(ABIBancorNetwork, BancorNetwork)
+    let amountReturn = await bancorNetwork.methods.getReturnByPath(
+      path,
+      toWei(String(amount))
+    ).call()
+
+    if(amountReturn){
+      amountReturn = Number(fromWei(hexToNumberString(amountReturn[0]._hex)))
+    }else{
+      amountReturn = 0
+    }
+
+    return amountReturn
+  }
+
+  // return from 1 in dai, to 1 in dai, from/to 1, and total trade value in DAI
+  getRateInfo = async (objPropsFrom, objPropsTo, web3) => {
     const pathFrom = getPath(this.props.from, "DAI", this.props.bancorTokensStorageJson, objPropsFrom)
     const pathTo = getPath(this.props.to, "DAI", this.props.bancorTokensStorageJson, objPropsTo)
     const pathFromTo = getPath(this.props.from, this.props.to, this.props.bancorTokensStorageJson, objPropsFrom, objPropsTo)
 
-    // get rate from in DAI
-    let amountReturnFrom = await bancorNetwork.methods.getReturnByPath(
-      pathFrom,
-      toWei(String(1))
-    ).call()
-
-    if(amountReturnFrom){
-      amountReturnFrom = Number(fromWei(hexToNumberString(amountReturnFrom[0]._hex)))
-    }else{
-      amountReturnFrom = 0
-    }
-
-    // get rate to in DAI
-    let amountReturnTo = await bancorNetwork.methods.getReturnByPath(
-      pathTo,
-      toWei(String(1))
-    ).call()
-
-    if(amountReturnTo){
-      amountReturnTo = Number(fromWei(hexToNumberString(amountReturnTo[0]._hex)))
-    }else{
-      amountReturnTo = 0
-    }
-
-    // get rate from/to
-    let amountReturnFromTo = await bancorNetwork.methods.getReturnByPath(
-      pathFromTo,
-      toWei(String(1))
-    ).call()
-
-    if(amountReturnFromTo){
-      amountReturnFromTo = Number(fromWei(hexToNumberString(amountReturnFromTo[0]._hex)))
-    }else{
-      amountReturnFromTo = 0
-    }
+    // get rate from in DAI for 1 token
+    const amountReturnFrom = await this.getReturnByPath(pathFrom, 1, web3)
+    // get rate from in DAI for 1 token
+    const amountReturnTo = await this.getReturnByPath(pathTo, 1, web3)
+    // get rate from/to 1 token
+    const amountReturnFromTo = await this.getReturnByPath(pathFromTo, 1, web3)
 
     // get Total trade value
     let totalTradeValue
-    if(this.props.amountReturn > 0){
-      let value = await bancorNetwork.methods.getReturnByPath(
-        pathTo,
-        toWei(String(this.props.amountReturn))
-      ).call()
-
-      if(value){
-        totalTradeValue = Number(fromWei(hexToNumberString(value[0]._hex)))
-      }else{
-        totalTradeValue = 0
-      }
-    }
+    if(this.props.amountReturn > 0)
+      totalTradeValue = await this.getReturnByPath(pathTo, this.props.amountReturn, web3)
 
     return { amountReturnFrom, amountReturnTo, amountReturnFromTo, totalTradeValue }
   }
 
-  // set state addreses to and from and user balance from
+  // set state addreses to and from and user balance, and direction rate data
   setTokensData = async () => {
-    if(this.props.to && this.props.from && this.props.web3 && this.props.accounts){
+    if(this.props.to && this.props.from){
       const { objPropsFrom, objPropsTo, sendFrom, sendTo } = getDirectionData(
         this.props.from,
         this.props.to,
@@ -123,8 +103,8 @@ class DirectionInfo extends Component {
         this.props.useERC20AsSelectFrom,
         this.props.useERC20AsSelectTo
       )
-      const web3 = this.props.web3
-      const { userBalanceFrom, balanceOfTo } = await this.getTokensBalance(sendFrom, sendTo, web3)
+      const web3 = getWeb3ForRead(this.props.web3)
+      const { userBalanceFrom, balanceOfTo } = this.props.accounts ? await this.getTokensBalance(sendFrom, sendTo, web3) : { userBalanceFrcom:0, balanceOfTo:0 }
       const { amountReturnFrom, amountReturnTo, amountReturnFromTo, totalTradeValue } = await this.getRateInfo(objPropsFrom, objPropsTo, web3)
 
       this.setState({ sendFrom, sendTo, userBalanceFrom, balanceOfTo, amountReturnFrom, amountReturnTo, amountReturnFromTo, totalTradeValue })
@@ -135,7 +115,6 @@ class DirectionInfo extends Component {
    return(
     <React.Fragment>
     {
-      /*Token info*/
       this.state.sendTo && this.state.sendFrom && this.props.directionAmount > 0
       ?
       (
@@ -144,12 +123,22 @@ class DirectionInfo extends Component {
       <Badge variant="Light">
       Etherscan: { <a href={EtherscanLink + "token/" + this.state.sendTo} target="_blank" rel="noopener noreferrer"> {this.props.to}</a> }
       </Badge>
-      <Badge variant="Light">
-      Your balance of {this.props.from}: {this.state.userBalanceFrom}
-      </Badge>
-      <Badge variant="Light">
-      Your balance of {this.props.to}: &nbsp; {this.state.balanceOfTo}
-      </Badge>
+      {
+        this.props.accounts
+        ?
+        (
+          <React.Fragment>
+          <Badge variant="Light">
+          Your balance of {this.props.from}: {this.state.userBalanceFrom}
+          </Badge>
+          <Badge variant="Light">
+          Your balance of {this.props.to}: &nbsp; {this.state.balanceOfTo}
+          </Badge>
+          </React.Fragment>
+        )
+        :
+        (null)
+      }
       <Badge variant="Light">
       Total trade value $: {this.state.totalTradeValue}
       </Badge>
