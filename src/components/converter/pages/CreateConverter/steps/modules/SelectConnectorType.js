3bae61405281287e9f68777c430a419834a39b6d
Ruslan <ruslan777mirov@gmail.com>
2019-09-23 09:06:26 +0300

    Check

---
+++ src/components/converter/pages/CreateConverter/steps/modules/SelectConnectorType.js
@@ -0,0 +1,22 @@
+// import React, { Component } from 'react'
+//
+// class SelectConnectorType extends Component {
+//
+//   render() {
+//     return (
+//       <Form style={{margin: '10px 0', maxWidth: '350px', width:'100%'}}>
+//       <Form.Group controlId="exampleForm.ControlSelect1">
+//       <Form.Label>Select connector type</Form.Label>
+//       <Form.Control as="select" onChange={(e) => this.setState({ connectorType:e.target.value })}>
+//         <option>...</option>
+//         <option>BNT</option>
+//         <option>USDB</option>
+//       </Form.Control>
+//       </Form.Group>
+//       </Form>
+//     )
+//   }
+//
+// }
+//
+// export default SelectConnectorType
