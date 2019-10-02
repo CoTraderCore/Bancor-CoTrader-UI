69cdecc924dc1ddeb692180c987d915de3000b1b
Ruslan <ruslan777mirov@gmail.com>
2019-10-02 11:12:06 +0300

    Replace USDB address

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const BancorGasLimit = "0x607a5C47978e2Eb6d59C6C6f51bc0bF411f4b85a"
 // MAINNET
 export const BancorRegistry = "0x52Ae12ABe5D8BD778BD5397F99cA900624CfADD4"
 export const BNTToken = "0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C"
-export const USDBToken = "0xd1146B08e8104EeDBa44a73B7bda1d102c6ceDC9"
+export const USDBToken = "0x309627af60f0926daa6041b8279484312f2bf060"
 export const ConvertersRegistryList = "0x7bdb720af9c0da53744aa007984031ceca528ad0"
 export const BancorNetwork = "0xeee90e509a639e95e3bb502b17a0eed6e014bfc0"
 export const BancorETH = "0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315"
