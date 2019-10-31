693504637f1c932adf0cdc9ec06404f1971ee5eb
Ruslan <ruslan777mirov@gmail.com>
2019-10-31 14:05:53 +0200

    CSS refactoring

--- src/components/converter/pages/TradePage.js
+++ src/components/converter/pages/TradePage.js
@@ -1,44 +1,12 @@
 import React from 'react'
 import TradeModal from '../modals/TradeModal'
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { ArrowRight } from '@material-ui/icons';
+import pageStyles from '../../../css/pageStyles'
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    minWidth: 275,
-    margin: theme.spacing(2, 0),
-    backgroundColor:'rgba(255,255,255,0.1)',
-    /*color:'rgba(255,255,255,0.8)',*/
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 22,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  icon: {
-    margin: theme.spacing(0),
-    fontSize: 25,
-    marginBottom: '-6px',
-  },
-  footer:{
-    borderTop: '1px solid #eee',
-  },
-  modal:{
-    margin: theme.spacing(2, 0),
-  },
-  button:{
-    margin: theme.spacing(1),
-  },
-}));
+const useStyles = pageStyles
 
 function TradePage() {
   const classes = useStyles();
