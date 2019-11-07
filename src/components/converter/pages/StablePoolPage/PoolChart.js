f63f8b5dc006f00caea674cac1f92432e8cb7ff2
Ruslan <ruslan777mirov@gmail.com>
2019-11-07 10:08:20 +0200

    Update stable pool page, not finished

--- src/components/converter/pages/StablePoolPage/PoolChart.js
+++ src/components/converter/pages/StablePoolPage/PoolChart.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
 
+function timeStempToDate(timestamp){
+  const date = new Date()
+  date.setTime(timestamp *1000)
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  return day + "." + month + "." + year + " " + hours + ":" + minutes + ":" + seconds
+}
+
 function PoolChart(props){
   console.log("props.data", props.data)
-  const timeLabels = props.data.roi.map(item => item.timestamp)
-  const chartData = props.data.roi.map(item => item.token_price_in_base_token)
+  const timeLabels = props.data.roi.map(item => timeStempToDate(item.timestamp))
+  const chartData = props.data.roi.map(item => item[props.property])
   const label = props.label
 
   const chartsData = {
@@ -12,30 +24,30 @@ function PoolChart(props){
       datasets: [
         {
           label,
-          backgroundColor: 'rgba(138,43,226, 0.4)',
+          backgroundColor: 'mediumslateblue',
           fill: false,
           lineTension: 0.1,
-          borderColor: 'rgba(138,43,226)',
+          borderColor: 'mediumslateblue',
           borderCapStyle: 'butt',
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(138,43,226)',
+          pointBorderColor: 'mediumslateblue',
           pointBackgroundColor: '#fff',
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(138,43,226)',
-          pointHoverBorderColor: 'rgba(138,43,226)',
+          pointHoverBackgroundColor: 'mediumslateblue',
+          pointHoverBorderColor: 'mediumslateblue',
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: chartData
+          data: chartData,
         }
       ]
   }
 
   return(
-    <div style={{ width: 210, height: 170 }}>
+    <div style={{ width: 320, height: 290 }}>
     <Line data={chartsData} />
     </div>
 
