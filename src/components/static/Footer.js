54a89c9a1f7fea4c8957962ed0cc1f007ac3a490
Ruslan <ruslan777mirov@gmail.com>
2019-11-12 15:36:55 +0200

    Fix cot link

--- src/components/static/Footer.js
+++ src/components/static/Footer.js
@@ -20,7 +20,7 @@ function Footer() {
     <Typography className={classes.footertext} color="textSecondary">
       DEX is free trade. Let freedom ring. This is a free, open source, unrestricted portal into the{" "}
       <a color="textprimary" href="http://Bancor.network" target="_blank" rel="noopener noreferrer">Bancor.network</a>,{" "}
-      made by <a color="textprimary" href="https://about.cotrader.com/" rel="noopener noreferrer" target="_blank">CoTrader</a>
+      made by <a color="textprimary" href="https://cotrader.com/" rel="noopener noreferrer" target="_blank">CoTrader</a>
     </Typography>
     <Typography className={classes.footertext} color="textSecondary">
       <a color="textprimary" href="https://t.me/cotrader" target="_blank" rel="noopener noreferrer"> Chat and Support</a>
