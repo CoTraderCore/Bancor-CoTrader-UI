5782b3c5927261ffc91a7ed2ba8c29dffe6de3c4
Ruslan <ruslan777mirov@gmail.com>
2019-11-21 15:20:02 +0200

    ADD V20 Converter

--- src/components/main/pages/CreateConverter/steps/StepThree.js
+++ src/components/main/pages/CreateConverter/steps/StepThree.js
@@ -10,8 +10,7 @@ import Button from '@material-ui/core/Button'
 import UserInfo from '../../../../templates/UserInfo'
 
 class StepThree extends Component {
-
- addConnector = async (tokenAddress) => {
+ addReserve = async (tokenAddress) => {
   const web3 = this.props.MobXStorage.web3
   const accounts = this.props.MobXStorage.accounts
   const converterHash = window.localStorage.getItem('txConverter')
@@ -20,10 +19,10 @@ class StepThree extends Component {
   if(converterInfo !== null && converterInfo !== "undefined"){
     window.localStorage.setItem('Converter', converterInfo.contractAddress)
     const converter = new web3.eth.Contract(ABIConverter, converterInfo.contractAddress)
-    console.log("PARAMS: ", window.localStorage.getItem('userToken'), 500000, false)
+    console.log("PARAMS: ", window.localStorage.getItem('userToken'), 500000)
     const gasPrice = this.props.MobXStorage.GasPrice
 
-    converter.methods.addConnector(window.localStorage.getItem('userToken'), 500000, false).send({
+    converter.methods.addReserve(window.localStorage.getItem('userToken'), 500000).send({
       from:accounts[0],
       gas:1372732,
       gasPrice
@@ -52,12 +51,12 @@ class StepThree extends Component {
     <strong>Add connector</strong>
     </Typography>
     <Typography variant="body1" className={'mb-2'} component="p">
-    This <UserInfo label="Bancor documentation" info="Existing ERC20 token address, Weight 500,000 (50%), enableVirtualB-alance - false (can be updated later on if needed)"/> step will be done
+    This <UserInfo label="Bancor documentation" info="Existing ERC20 token address, ratio ()constant reserve ratio, represented in ppm, 1-1000000"/> step will be done
     </Typography>
     <Typography className={'mt-2 mb-2'} component="div">
     <hr/>
-    <Form style={{margin: '10px auto', maxWidth: '350px', width:'100%'}}>
-    <Button variant="contained" color="primary" size="medium" onClick={() => this.addConnector()}>add connector</Button>
+    <Form style={{margin: '10px 0', maxWidth: '350px', width:'100%'}}>
+    <Button variant="contained" color="primary" size="medium" onClick={() => this.addReserve()}>add reserve</Button>
     </Form>
     </Typography>
     </CardContent>
